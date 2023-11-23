113bbf37c8ebb6e9059d0c68a719e7f72c509fe1
DariaDon <dasha.dd55@gmail.com>
2023-11-23 18:48:59 +0500

    LR3

--- MySecondTestAppSpringBoot/src/main/java/ru/don/MySecondTestAppSpringBoot/controller/MyController.java
+++ MySecondTestAppSpringBoot/src/main/java/ru/don/MySecondTestAppSpringBoot/controller/MyController.java
@@ -5,27 +5,33 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import ru.don.MySecondTestAppSpringBoot.model.Request;
-import ru.don.MySecondTestAppSpringBoot.model.Response;
+import ru.don.MySecondTestAppSpringBoot.exception.*;
+import ru.don.MySecondTestAppSpringBoot.model.*;
 import ru.don.MySecondTestAppSpringBoot.service.ValidationService;
-import ru.don.MySecondTestAppSpringBoot.exception.ValidationFailedException;
-import ru.don.MySecondTestAppSpringBoot.exception.UnsupportedCodeException;
 import ru.don.MySecondTestAppSpringBoot.service.UnsupportedCodeValidation;
+import ru.don.MySecondTestAppSpringBoot.service.ModifyResponseService;
+import ru.don.MySecondTestAppSpringBoot.util.DateTimeUtil;
 import org.springframework.validation.BindingResult;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import javax.validation.Valid;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RestController
 public class MyController {
     private final ValidationService validationService;
+    private final ModifyResponseService modifyResponseService;
     private final UnsupportedCodeValidation unsupportedCodeValidation;
 
     @Autowired
-    public MyController(ValidationService validationService, UnsupportedCodeValidation unsupportedCodeValidation) {
+    public MyController(ValidationService validationService, UnsupportedCodeValidation unsupportedCodeValidation,
+                        @Qualifier("ModifySystemTimeResponseService") ModifyResponseService modifyResponseService) {
         this.validationService = validationService;
         this.unsupportedCodeValidation = unsupportedCodeValidation;
+        this.modifyResponseService = modifyResponseService;
     }
 
     @PostMapping(value = "/feedback")
@@ -34,38 +40,54 @@ public class MyController {
 
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
 
+        log.info("request: {}", request);
+
         Response response = Response.builder()
                 .uid(request.getUid())
                 .operationUid(request.getOperationUid())
-                .systemTime(simpleDateFormat.format(new Date()))
-                .code("success")
-                .errorCode("")
-                .errorMessage("")
+                .systemTime(DateTimeUtil.getCustomFormat().format(new Date()))
+//                .systemTime(simpleDateFormat.format(new Date()))
+                .code(Codes.SUCCESS)
+                .errorCode(ErrorCodes.EMPTY)
+                .errorMessage(ErrorMessages.EMPTY)
                 .build();
 
         try {
             validationService.isValid(bindingResult);
         } catch (ValidationFailedException e) {
-            response.setCode("failed");
-            response.setErrorCode("ValidationException");
-            response.setErrorMessage("Ошибка валидации");
+            log.error("error: {}", e.getMessage());
+            response.setCode(Codes.FAILED);
+//            response.setCode("failed");
+            response.setErrorCode(ErrorCodes.VALIDATION_EXCEPTION);
+//            response.setErrorCode("ValidationException");
+            response.setErrorMessage(ErrorMessages.VALIDATION);
+//            response.setErrorMessage("Ошибка валидации");
             return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
         } catch (Exception e) {
-            response.setCode("failed");
-            response.setErrorCode("UnknownException");
-            response.setErrorMessage("Произошла непредвиденная ошибка");
+            log.error("error: {}", e.getMessage());
+            response.setCode(Codes.FAILED);
+//            response.setCode("failed");
+            response.setErrorCode(ErrorCodes.UNKNOWN_EXCEPTION);
+//            response.setErrorCode("UnknownException");
+            response.setErrorMessage(ErrorMessages.UNKNOWN);
+//            response.setErrorMessage("Произошла непредвиденная ошибка");
             return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
         try {
             unsupportedCodeValidation.Valid(Integer.parseInt(request.getUid()));
         } catch (UnsupportedCodeException e) {
-            response.setCode("failed");
-            response.setErrorCode("UnsupportedCodeException");
-            response.setErrorMessage("uid = 123");
+            log.error("error: {}", e.getMessage());
+            response.setCode(Codes.FAILED);
+//            response.setCode("failed");
+            response.setErrorCode(ErrorCodes.UNSUPPORTED_EXCEPTION);
+//            response.setErrorCode("UnsupportedCodeException");
+            response.setErrorMessage(ErrorMessages.UNSUPPORTED);
+//            response.setErrorMessage("uid = 123");
             return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
         }
-
+        modifyResponseService.modify(response);
+        log.info("response: {}", response);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
--- MySecondTestAppSpringBoot/src/main/java/ru/don/MySecondTestAppSpringBoot/model/Request.java
+++ MySecondTestAppSpringBoot/src/main/java/ru/don/MySecondTestAppSpringBoot/model/Request.java
@@ -19,7 +19,7 @@ public class Request {
     @Size(max = 32)
     private String operationUid;
 
-    private String systemName;
+    private System systemName;
 
     @NotBlank
     private String systemTime;
@@ -35,4 +35,19 @@ public class Request {
     private int productCode;
 
     private int smsCode;
+
+    @Override
+    public String toString() {
+        return "{" +
+                "uid='" + uid + '\'' +
+                ", opertionUid='" + operationUid + '\'' +
+                ", systemName='" + systemName + '\'' +
+                ", systemTime='" + systemTime + '\'' +
+                ", source='" + source + '\'' +
+                ", communicationId=" + communicationId +
+                ", templateId=" + templateId +
+                ", productCode=" + productCode +
+                ", smsCode=" + smsCode +
+                '}';
+    }
 }
